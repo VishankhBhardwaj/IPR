194ae32048c46cb0e06dbf801664bc4eb0a9663f
Kush Aheer <kushaheer709@gmail.com>
2026-08-11 23:47:02 +0530

    Added Column in a table

---
+++ Backend/prisma/migrations/20260811181501_changed_schema/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `institueAffiliation` on the `patent` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `patent` DROP COLUMN `institueAffiliation`;
+
+-- AlterTable
+ALTER TABLE `patentinventor` ADD COLUMN `instituteAffiliation` TEXT NOT NULL DEFAULT '';
+
+-- AddForeignKey
+ALTER TABLE `Patent` ADD CONSTRAINT `Patent_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `PatentInventor` ADD CONSTRAINT `PatentInventor_patentId_fkey` FOREIGN KEY (`patentId`) REFERENCES `Patent`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `InventorDepartment` ADD CONSTRAINT `InventorDepartment_inventorId_fkey` FOREIGN KEY (`inventorId`) REFERENCES `PatentInventor`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `InventorDepartment` ADD CONSTRAINT `InventorDepartment_departmentId_fkey` FOREIGN KEY (`departmentId`) REFERENCES `Department`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
